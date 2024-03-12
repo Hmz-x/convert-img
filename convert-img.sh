0560b1664f203d40b9062649e0e1a902d4bd04b9
Hmz-x <hamzamusic34@gmail.com>
2024-03-12 03:43:46 -0400

    has all mirroring functionality

--- convert-img.sh
+++ convert-img.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mirror_vertically()
+mirror_vertically_gravity_south()
 {
 	input="$1"
 	convert "$input" -gravity South -chop 0x50% -flip -write mpr:bottom +delete \
@@ -11,11 +11,42 @@ mirror_vertically()
 mirror_vertically_gravity_north()
 {
 	input="$1"
-	convert "$input" -gravity South -chop 0x50% -flip -write mpr:bottom +delete \
-		"$input" -gravity North -chop 0x50% -write mpr:top +delete \
+	convert "$input" -gravity North -chop 0x50% -flip -write mpr:top +delete \
+		"$input" -gravity North -chop 0x50% -write mpr:bottom +delete \
 		-append mpr:top mpr:bottom "$input"
 }
 
+mirror_horizontally_gravity_west()
+{
+	input="$1"
+	convert "$input" -gravity West -chop 50%x0 -flop -write mpr:right +delete \
+		"$input" -gravity West -chop 50%x0 -write mpr:left +delete \
+		+append mpr:left mpr:right "out-$input"
+}
 
-mirror_vertically "$1"
+mirror_horizontally_gravity_west()
+{
+	input="$1"
+	convert "$input" -gravity West -chop 50%x0 -flop -write mpr:right +delete \
+		"$input" -gravity West -chop 50%x0 -write mpr:left +delete \
+		+append mpr:left mpr:right "out-$input"
+}
+
+mirror_horizontally_gravity_east()
+{
+	input="$1"
+	convert "$input" -gravity East -chop 50%x0 -flop -write mpr:left +delete \
+		"$input" -gravity East -chop 50%x0 -write mpr:right +delete \
+		+append mpr:left mpr:right "out-$input"
+}
+
+resize_item_img()
+{
+	input="$1"
+	#convert "$input" 
+}
+
+
+#mirror_vertically "$1"
 #mirror_vertically_gravity_north "$1"
+#mirror_horizontally_gravity_west "$1"
