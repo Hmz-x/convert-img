23df017c79223a459a69d0daf8c272b6e69b4d7b
Hmz-x <hamzamusic34@gmail.com>
2024-03-12 20:35:03 -0400

    added todo

--- convert-img.sh
+++ convert-img.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# TO IMPLEMENT:
+# 0) input string validation for user input
+# 1) transparent "item" images to be insterted on the main input image,
+# implemented with features such as slight rotation, color enchacement, easy duplication placement not to mention resized
+# 2) Random functionality implementation: setting defaults for number of fx, etc.
+# 2) Output to be automatically sent to the server directory and the website to display output image. The output
+# image to be then downloadable after clicking on it.
+# Implement special start shape and star formations: make em stack side by side and top and bottom
+# Client to send image via signal ? and for automatic downloading and running randomization and sending to server
+
+
 mirror_vertically_gravity_south()
 {
 	input="$1"
@@ -14,14 +25,6 @@ mirror_vertically_gravity_north()
 	convert "$input" -gravity North -chop 0x50% -flip -write mpr:top +delete \
 		"$input" -gravity North -chop 0x50% -write mpr:bottom +delete \
 		-append mpr:top mpr:bottom "$input"
-}
-
-mirror_horizontally_gravity_west()
-{
-	input="$1"
-	convert "$input" -gravity West -chop 50%x0 -flop -write mpr:right +delete \
-		"$input" -gravity West -chop 50%x0 -write mpr:left +delete \
-		+append mpr:left mpr:right "out-$input"
 }
 
 mirror_horizontally_gravity_west()
@@ -49,4 +52,4 @@ resize_item_img()
 
 #mirror_vertically "$1"
 #mirror_vertically_gravity_north "$1"
-#mirror_horizontally_gravity_west "$1"
+mirror_horizontally_gravity_west "$1"
